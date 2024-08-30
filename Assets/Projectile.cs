9f08ee746e9b9307efe7d1a6ce257142c190c81f
Angelo Maynigo <angelomaynigo@gmail.com>
2024-08-30 06:09:37 -0700

    The projectile will now delete itself based on how far it gets from the player position rather than the camera position

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public Rigidbody body;
+    [SerializeField] private Rigidbody body;
     private float bulletSpeed = 35.0f;
 
-    private Camera cam;
+    private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent<Rigidbody>();
-        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         // Delete the projectile when out of camera view
-        if (Mathf.Abs(transform.position.x) > Mathf.Abs(cam.transform.position.x) + 10.0f 
-            || Mathf.Abs(transform.position.z) > Mathf.Abs(cam.transform.position.z) + 10.0f) {
+        if (Mathf.Abs(transform.position.x) > Mathf.Abs(player.transform.position.x) + 20.0f 
+            || Mathf.Abs(transform.position.z) > Mathf.Abs(player.transform.position.z) + 20.0f) {
             Destroy(gameObject);
         }
     }
