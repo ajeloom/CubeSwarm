ccf9eb524bce2b64667887c88d2cdab1fd6661ed
Angelo Maynigo <angelomaynigo@gmail.com>
2024-08-31 06:33:39 -0700

    The zombie enemy will move and turn towards the player

---
+++ Assets/Zombie.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Timeline;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move towards the player
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        
+        // Turn towards the player
+        float xDist = player.transform.position.x - transform.position.x;
+        float zDist = -player.transform.position.z + transform.position.z;
+        float turn = Mathf.Atan2(zDist, xDist) * Mathf.Rad2Deg + 90.0f;
+        transform.rotation = Quaternion.Euler(0, turn, 0);
+    }
+}
